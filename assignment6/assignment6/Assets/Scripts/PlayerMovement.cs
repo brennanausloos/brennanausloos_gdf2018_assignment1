1755e4df85053f097d738035bddfab5afb2f5bc0
Brennan Ausloos <bma16b@my.fsu.edu>
2018-09-26 12:19:00 -0400

    Assignment 6 UFO movement 1.0

---
+++ assignment6/assignment6/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    // Use this for initialization
+    public float speed;
+
+    private Rigidbody2D rb2d;
+
+    void Start()
+    {
+
+        rb2d = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        rb2d.AddForce(movement * speed);
+    }
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+        }
+
+    }
+}
